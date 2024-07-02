c01c23f2d5a7b61ae46586ec9cc3e10c92683cf3
fekoneko <55813967+fekoneko@users.noreply.github.com>
2024-07-02 12:58:50 +0300

    test layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
-import { FC } from 'react';
+import ollama from 'ollama/browser';
+import { FC, FormEvent, useState } from 'react';
+import styles from './App.module.css';
 
 export const App: FC = () => {
-  return <div>App</div>;
+  const [prompt, setPrompt] = useState<string>('');
+  const [response, setResponse] = useState<string>();
+
+  const askAi = async (prompt: string) => {
+    setResponse(undefined);
+    const newResponse = await ollama.chat({
+      model: 'llama3',
+      messages: [{ role: 'user', content: prompt }],
+    });
+    setResponse(newResponse.message.content);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    askAi(prompt);
+  };
+
+  return (
+    <div className={styles['chat-container']}>
+      <p className={styles['chat']}>{response}</p>
+
+      <form onSubmit={handleSubmit} className={styles['prompt']}>
+        <input
+          onChange={(e) => setPrompt(e.target.value)}
+          type="text"
+          placeholder="Enter your prompt..."
+        />
+        <button type="submit">Ask AI</button>
+      </form>
+    </div>
+  );
 };
