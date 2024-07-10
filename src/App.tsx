ad8b1ed9f76ea766fbd780dc055121b751501059
fekoneko <55813967+fekoneko@users.noreply.github.com>
2024-07-10 20:37:53 +0300

    moved `ChatPage` wrappers to new `Shell` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
+import { Shell } from '@/components/Shell';
 import { ChatPage } from '@/pages/ChatPage';
 import { FC } from 'react';
-import { AppShell } from '@mantine/core';
-import styles from './App.module.css';
 
 export const App: FC = () => {
   return (
-    <AppShell>
-      <AppShell.Main className={styles.main}>
-        <ChatPage />
-      </AppShell.Main>
-    </AppShell>
+    <Shell>
+      <ChatPage />
+    </Shell>
   );
 };
