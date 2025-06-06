f69069131c16fce9ae81579f992af3e9b1e3f4c3
fekoneko <55813967+fekoneko@users.noreply.github.com>
2025-06-06 15:12:26 +0300

    add little ollama sitting at the top of the page

--- src/features/messenger/ui/SidePanel/SidePanel.tsx
+++ src/features/messenger/ui/SidePanel/SidePanel.tsx
@@ -1,3 +1,4 @@
+import { OllamaLogo } from "@/assets/OllamaLogo";
 import { ChatList } from "@/features/messenger/ui/ChatList";
 import { ThemeToggle } from "@/features/messenger/ui/ThemeToggle";
 import { FC } from "react";
@@ -6,6 +7,7 @@ import classes from "./SidePanel.module.css";
 export const SidePanel: FC = () => (
   <div className={classes.sidePanel}>
     <div className={classes.titleContainer}>
+      <OllamaLogo size="3rem" className={classes.logo} />
       <h1 role="banner" className={classes.title}>
         Ollama UI
       </h1>
